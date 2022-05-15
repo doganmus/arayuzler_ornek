ba223ab694ee5fe97f94a453acfb9c19e0d93054
Dogan Mus <doganmus87@gmail.com>
2022-05-15 17:46:12 +0300

    project craeted

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arayuzler_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
